27a888dae388d7cf6fe37211f6cdd278d72b1272
Kyungrae <jeremiah.kim@hotmail.com>
2019-10-29 11:13:29 -0700

    basic MVC completed

---
+++ MyFirstMVCApp/MyFirstMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstMVCApp.Models;
+
+namespace MyFirstMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int yearFrom, int yearTo)
+        {
+            TimePerson list = new TimePerson(yearFrom, yearTo);
+            return RedirectToAction("Results", list);
+        }
+
+        public ViewResult Results(TimePerson list)
+        {
+            return View(list);
+        }
+    }
+}
---
+++ MyFirstMVCApp/MyFirstMVCApp/Models/TimePerson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace MyFirstMVCApp.Models
+{
+    public class TimePerson
+    {
+        public int Year { get; set; }
+        public string Honor { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int BirthYear { get; set; }
+        public int DeathYear { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Context { get; set; }
+
+        public TimePerson()
+        {
+
+        }
+    }
+}
